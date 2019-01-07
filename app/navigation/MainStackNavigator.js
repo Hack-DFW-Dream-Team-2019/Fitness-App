1af8ea5e850ceb0d29849c536e390890b012d374
Caleb Trachte <cetrachte@hotmail.com>
2019-01-07 10:10:16 -0600

    add color palette, add firebase config to MainStackNavigator, move when we have a menu component.

--- app/navigation/MainStackNavigator.js
+++ app/navigation/MainStackNavigator.js
@@ -5,6 +5,19 @@ import LoginScreen from '../screens/Login';
 import AuthScreen from '../screens/Auth';
 import RegisterScreen from '../screens/Register';
 
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBjo5v_7DazFSDzqoR6daYqtlxZ7KL0sn4",
+  authDomain: "fitness-app-3d4cd.firebaseapp.com",
+  databaseURL: "https://fitness-app-3d4cd.firebaseio.com",
+  projectId: "fitness-app-3d4cd",
+  storageBucket: "fitness-app-3d4cd.appspot.com",
+  messagingSenderId: "399198279886"
+};
+firebase.initializeApp(firebaseConfig);
+
 const MainStackNavigation = createStackNavigator({
     Login: {
       screen: LoginScreen
