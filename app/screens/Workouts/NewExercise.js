d34243f353137f359e26eb199362c493876318d5
Caleb Trachte <cetrachte@hotmail.com>
2019-01-14 16:36:44 -0600

    copy default styling from welcome screen as placeholder

--- app/screens/Workouts/NewExercise.js
+++ app/screens/Workouts/NewExercise.js
@@ -35,4 +35,35 @@ export default class EditExercise extends Component {
 }
 
 const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    header: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    body: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+
+        marginVertical: 10,
+        alignItems: 'center',
+        minWidth: 200,
+        paddingVertical: 20,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#707070'
+
+    },
+    buttonText:{
+        textTransform: 'uppercase',
+        color: "#707070",
+    }
 });
