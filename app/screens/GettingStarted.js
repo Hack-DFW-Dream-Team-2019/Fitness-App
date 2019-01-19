6af5f92f1d84a7dd861cea98c759486aeb03469b
Richard Huynh <rxh143030@utdallas.edu>
2019-01-19 16:22:48 -0600

    Added Swiper to getting started

--- app/screens/GettingStarted.js
+++ app/screens/GettingStarted.js
@@ -1,23 +1,62 @@
 import React, {Component} from "react";
-import { Button, Container, Header, Content, Footer, View, Text } from 'native-base';
+import { Button, Container, Header, Content, Footer, View, Text, Icon } from 'native-base';
 import { Image, StyleSheet, TouchableOpacity} from 'react-native';
+
+import Swiper from 'react-native-swiper';
 
 
 export default class Login extends Component {
 
-  // Navigation Options
-  static navigationOptions = {
-    
-  };
+	constructor(props){
+		super(props)
+		this.state = {
 
-  render() {
-    return (
-        <View style={style.container}>
-            <Text>This is the getting started screen!</Text>
-        </View>
-    );
-  }
-}
+		}
+	}
+
+	// Navigation Options
+	static navigationOptions = {
+		header: null
+	};
+
+	render() {
+
+		const day = new Date().getDay()
+		const weekday = new Array(7);
+
+		weekday[0] =  "Sunday";
+		weekday[1] = "Monday";
+		weekday[2] = "Tuesday";
+		weekday[3] = "Wednesday";
+		weekday[4] = "Thursday";
+		weekday[5] = "Friday";
+		weekday[6] = "Saturday";
+
+		return (
+			<View style={{flex: 1}}>
+				<View style={style.header}>
+					<TouchableOpacity
+						style={style.icon}
+						>
+						<Icon type="MaterialIcons" name="menu" />
+					</TouchableOpacity>
+					<Text style={{color: "#707070"}}>{weekday[day]}</Text>
+				</View>
+				<Swiper activeDotColor="#000"showsButtons={false}>
+					<View style={style.slide}>
+						<Text>Hello Swiper</Text>
+					</View>
+					<View style={style.slide}>
+						<Text>Beautiful</Text>
+					</View>
+					<View style={style.slide}>
+						<Text>And simple</Text>
+					</View>
+				</Swiper>
+			</View>
+			);
+		}
+	}
 
 const style = StyleSheet.create({
   container: {
@@ -26,5 +65,23 @@ const style = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: '#fff',
+  },
+  header:{
+	alignItems: 'center',
+    marginTop: 15,
+    padding: 20
+  },
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  icon:{
+	  color: "#707070",
+	  position: 'absolute',
+	  top: 15,
+	  left: 10,
+	  fontSize: 30,
   }
 });
